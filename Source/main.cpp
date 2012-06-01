92a0509dfe22150c6b1ac3b1db382c0d1cc8ef54
Ming Chuang <ming@cs.jhu.edu>
2012-06-01 00:47:53 -0400

    test

--- Source/main.cpp
+++ Source/main.cpp
@@ -1,34 +1,10 @@
 #include <QApplication>
-#include <iostream>
 #include "myWindow.h"
-#include "geometry.h"
 #include "CmdLineParser.h"
 
-#include <qfile.h>
-#include <qtextstream.h>
-#include <QHash>
-#include <vector>
-
-//
-// For CVT
-//
-# include <cstdlib>
-# include <cmath>
-# include <ctime>
-# include <iostream>
-# include <iomanip>
-# include <cstring>
-//using namespace std;
-//# include "cvt.h"
-
-
-//////////////
-// Comments //
-//////////////
-cmdLineReadable       ReadURL( "url" );
-cmdLineString         Image( "img" ) , Annotation( "ano" ) , ImageUrl( "imgUrl" ) , AnnotationUrl( "anoUrl" );
+cmdLineString         Image( "img" ) , Annotation( "ano" );
 cmdLineIntArray< 3 >  DataRes( "dRes" );
-cmdLineReadable* params[]={ &ReadURL , &Image , &Annotation , &ImageUrl , &AnnotationUrl , &DataRes };
+cmdLineReadable* params[]={ &Image , &Annotation , &DataRes };
 
 void ShowUsage( char* ex )
 {
@@ -36,42 +12,19 @@ void ShowUsage( char* ex )
         printf( "Usage: %s\n" , ex );
         printf( "\n");
         printf( "\t--%s\t<X-res> <Y-res> <Z-res>\n" , DataRes.name    );
-        printf( "\t--%s/%s\t<input 3D image>       \n" , Image.name      , ImageUrl.name      );
-        printf( "\t--%s/%s\t<input 3D annotation>  \n" , Annotation.name , AnnotationUrl.name );
+        printf( "\t--%s\t<input 3D image>       \n" , Image.name      );
+        printf( "\t--%s\t<input 3D annotation>  \n" , Annotation.name );
 }
-
 
 int main( int argc , char *argv[] )
 {
-/*
-    std::vector< std::vector< float > > colors;
-    getRandomColors( 1000 , colors , true );
-return EXIT_SUCCESS;
-*/
-
-
     cmdLineParse( argc-1 , &argv[1] , sizeof(params) / sizeof(cmdLineReadable*) , params , 0 );
 
-    if( ReadURL.set )
+    if( !DataRes.set || !Image.set || !Annotation.set )
     {
-        std::string anoString;
-        printf("Annotation URL : ");
-        std::cin>>anoString;
-        AnnotationUrl.value = (char*)anoString.c_str();
-
-        std::string imgString;
-        printf("3D Image URL   : ");
-        std::cin>>imgString;
-        ImageUrl.value = (char*)imgString.c_str();
-
-        AnnotationUrl.set = ImageUrl.set = true;
-    }
-
-    if( !DataRes.set || (!Image.set&&!ImageUrl.set) || (!Annotation.set&&!AnnotationUrl.set)  )
-    {
-        if( !Image.set      && !ImageUrl.set      ) printf("[ERROR] did not specify the image file...\n"     );
-        if( !Annotation.set && !AnnotationUrl.set ) printf("[ERROR] did not specify the annotation file...\n");
-        if( !DataRes.set                          ) printf("[ERROR] did not specify the data resolution...\n");
+        if( !Image.set      ) printf("[ERROR] did not specify the image file...\n"     );
+        if( !Annotation.set ) printf("[ERROR] did not specify the annotation file...\n");
+        if( !DataRes.set    ) printf("[ERROR] did not specify the data resolution...\n");
         ShowUsage( argv[0] );
         return EXIT_FAILURE;
     }
@@ -84,22 +37,6 @@ return EXIT_SUCCESS;
                 params[i]->writeValue( valueString );
                 printf( "\t--%s %s \n" , params[i]->name , valueString );
         }
-
-    // unbelievable bug.... system() messed up AnnotationUrl.value.....
-    char imgCommand[512];
-    if( ImageUrl.set )
-    {
-        sprintf( imgCommand , "hdf5MakePNGs_ming.py %s img.data" , ImageUrl.value      );
-        Image.value      = (char*)"img.data";
-    }
-    char anoCommand[512];
-    if( AnnotationUrl.set )
-    {
-        sprintf( anoCommand , "hdf5MakePNGs_ming.py %s ano.data" , AnnotationUrl.value );
-        Annotation.value = (char*)"ano.data";
-    }
-    if( ImageUrl.set      ) system( imgCommand );
-    if( AnnotationUrl.set ) system( anoCommand );
 
     QApplication app( argc , argv );
     myWindow window( Image.value , Annotation.value , DataRes.values[0] , DataRes.values[1] , DataRes.values[2] );
