247e8820bf4bd17061abc3d9f4c3c55d675fa5f1
Juneki Hong <juneki@jhu.edu>
2012-08-21 14:55:50 -0400

    tweaked the .pro file to compile and run on the Linux Ugrad machines

--- Source/VolumeViewer.pro
+++ Source/VolumeViewer.pro
@@ -1,14 +1,14 @@
 #DEFINES += GLEWPATH_UNIX=/home/jhong/Documents/glew-1.7.0/lib
 
 TEMPLATE = app
-#QT += opengl
+QT += opengl
 CONFIG += console
 
 
 # This line manually links this code to your libGLEW library.
 # Please point it to where libGLEW is located in your machine.
 #LIBS += C:/QtSDK/glew-1.5.2/lib/libglew32.dll.a
-#LIBS += /home/jhong/Documents/glew-1.7.0/lib/libGLEW.a -lGLU
+LIBS += /home/jhong/Documents/glew-1.7.0/lib/libGLEW.a -lGLU
 
 
 # The cache library that I was using requires Boost. In order to get Boost running, I manually linked to it here.
@@ -23,49 +23,53 @@ LIBS += /usr/lib/libboost_thread-mt.so
 QMAKE_CXXFLAGS += -w
 
 
-#INCLUDEPATH += .
-#HEADERS += ./myWindow.h	\
-#    ControlPanel.h 	\
-#    glCamera.h 		\
-#    geometry.h 		\
-#    CmdLineParser.h \
-#    DynamicVoxelHierarchy/VoxelOracle.h \
-#    DynamicVoxelHierarchy/voxel.h \
-#    DynamicVoxelHierarchy/CacheMaintain.h \
-#    DynamicVoxelHierarchy/HierarchyRenderer.h\
-#    \
-#    DynamicVoxelHierarchy/lru_cache/src/lru_cache.h
+INCLUDEPATH += .
+HEADERS += ./myWindow.h	\
+    ControlPanel.h 	\
+    glCamera.h 		\
+    geometry.h 		\
+    CmdLineParser.h \
+    DynamicVoxelHierarchy/VoxelOracle.h \
+    DynamicVoxelHierarchy/voxel.h \
+    DynamicVoxelHierarchy/CacheMaintain.h \
+    DynamicVoxelHierarchy/HierarchyRenderer.h\
+    DynamicVoxelHierarchy/voxelDatabase.h \
+    DynamicVoxelHierarchy/timer.h \
+    \
+    DynamicVoxelHierarchy/lru_cache/src/lru_cache.h
 
-#SOURCES += ./main.cpp 	\
-#    myWindow.cpp 	\
-#    ControlPanel.cpp 	\
-#    glCamera.cpp 	\
-#    geometry.cpp 	\
-#    CmdLineParser.cpp \
-#    DynamicVoxelHierarchy/VoxelOracle.cpp \
-#    DynamicVoxelHierarchy/voxel.cpp \
-#    DynamicVoxelHierarchy/CacheMaintain.cpp \
-#    DynamicVoxelHierarchy/HierarchyRenderer.cpp\
-#    \
-#    DynamicVoxelHierarchy/lru_cache/src/lru_cache.cpp
+SOURCES += ./main.cpp 	\
+    myWindow.cpp 	\
+    ControlPanel.cpp 	\
+    glCamera.cpp 	\
+    geometry.cpp 	\
+    CmdLineParser.cpp \
+    DynamicVoxelHierarchy/VoxelOracle.cpp \
+    DynamicVoxelHierarchy/voxel.cpp \
+    DynamicVoxelHierarchy/CacheMaintain.cpp \
+    DynamicVoxelHierarchy/HierarchyRenderer.cpp\
+    DynamicVoxelHierarchy/voxelDatabase.cpp \
+    DynamicVoxelHierarchy/timer.cpp \
+    \
+    DynamicVoxelHierarchy/lru_cache/src/lru_cache.cpp
 
 
 
 #LIBS += /home/juneki/Documents/school/Research/glew-1.7.0/lib/libGLEW.a -lGLU
 
-INCLUDEPATH += .
-HEADERS += ./DynamicVoxelHierarchy/voxel.h \
-    DynamicVoxelHierarchy/CacheMaintain.h \
-    DynamicVoxelHierarchy/VoxelOracle.h \
-    DynamicVoxelHierarchy/HierarchyRenderer.h \
-    geometry.h \
-    DynamicVoxelHierarchy/voxelDatabase.h \
-    DynamicVoxelHierarchy/timer.h
-SOURCES += ./main.cpp 	\
-    DynamicVoxelHierarchy/voxel.cpp \
-    DynamicVoxelHierarchy/CacheMaintain.cpp \
-    DynamicVoxelHierarchy/VoxelOracle.cpp \
-    DynamicVoxelHierarchy/HierarchyRenderer.cpp \
-    geometry.cpp \
-    DynamicVoxelHierarchy/voxelDatabase.cpp \
-    DynamicVoxelHierarchy/timer.cpp
+#INCLUDEPATH += .
+#HEADERS += ./DynamicVoxelHierarchy/voxel.h \
+#    DynamicVoxelHierarchy/CacheMaintain.h \
+#    DynamicVoxelHierarchy/VoxelOracle.h \
+#    DynamicVoxelHierarchy/HierarchyRenderer.h \
+#    geometry.h \
+#    DynamicVoxelHierarchy/voxelDatabase.h \
+#    DynamicVoxelHierarchy/timer.h
+#SOURCES += ./main.cpp 	\
+#    DynamicVoxelHierarchy/voxel.cpp \
+#    DynamicVoxelHierarchy/CacheMaintain.cpp \
+#    DynamicVoxelHierarchy/VoxelOracle.cpp \
+#    DynamicVoxelHierarchy/HierarchyRenderer.cpp \
+#    geometry.cpp \
+#    DynamicVoxelHierarchy/voxelDatabase.cpp \
+#    DynamicVoxelHierarchy/timer.cpp
--- Source/main.cpp
+++ Source/main.cpp
@@ -1,3 +1,4 @@
+
 #include <GL/glew.h>
 
 #include <QApplication>
@@ -9,8 +10,24 @@ cmdLineIntArray< 3 >  DataRes( "dRes" );
 cmdLineReadable* params[]={ &Image , &Annotation , &DataRes };
 
 
+
 #include "DynamicVoxelHierarchy/HierarchyRenderer.h"
 #include "DynamicVoxelHierarchy/CacheMaintain.h"
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using std::string;
+using std::ifstream;
+using std::cout;
+using std::endl;
+
+
+void test1();
+void test2();
+void test3();
+
 
 
 
@@ -24,14 +41,16 @@ void ShowUsage( char* ex )
         printf( "\t--%s\t<input 3D annotation>  \n" , Annotation.name );
 }
 
+
 int main( int argc , char *argv[] )
 {
-    /*
-    cmdLineParse( argc-1 , &argv[1] , sizeof(params) / sizeof(cmdLineReadable*) , params , 0 );
+
+    cmdLineParse( argc-1 , &argv[1] , sizeof(params) /
+sizeof(cmdLineReadable*) , params , 0 );
 
     if( !DataRes.set || !Image.set || !Annotation.set )
     {
-        if( !Image.set      ) printf("[ERROR] did not specify the image file...\n"     );
+        if( !Image.set      ) printf("[ERROR] did not specify the image file...\n");
         if( !Annotation.set ) printf("[ERROR] did not specify the annotation file...\n");
         if( !DataRes.set    ) printf("[ERROR] did not specify the data resolution...\n");
         ShowUsage( argv[0] );
@@ -50,17 +69,117 @@ int main( int argc , char *argv[] )
     glewInit();
 
     QApplication app( argc , argv );
-    myWindow window( Image.value , Annotation.value , DataRes.values[0] , DataRes.values[1] , DataRes.values[2] );
+    myWindow window( Image.value , Annotation.value ,
+DataRes.values[0] , DataRes.values[1] , DataRes.values[2] );
     window.show();
     return app.exec();
 
-*/
+
 
     //--------------------------------------------------------
 
+    //test2();
+
+    //test3();
+}
 
 
+void test1()
+{
     CacheMaintain *cachemaintain = new CacheMaintain();
 
+    string a = "one";
+    string b = "two";
+
+    cachemaintain->insertVoxelData(a,Voxel(a),2);
+
+    Voxel v = cachemaintain->getVoxelData(a);
+    Voxel v2 = cachemaintain->getVoxelData(a);
+
+    cachemaintain->printCache();
+}
+
+void test2()
+{
+    // Voxel Oracle
+    int zoomedIn = 0;
+    Plane3D viewingPlane = Plane3D();
+    VoxelOracle* voxeloracle = new VoxelOracle(zoomedIn,viewingPlane);
+
+    //Cache maintain
+    int cacheCapacity = 10;
+    int hierarchySize = 10;
+    CacheMaintain* cachemaintain = new
+CacheMaintain(cacheCapacity,hierarchySize);
+
+    //Hierarchy Renderer
+    HierarchyRenderer* hierarchyRenderer = new
+HierarchyRenderer(voxeloracle,cachemaintain);
+
+
+
+    voxeloracle->setZoomedIn(0);
+    std::vector<Voxel>* voxelsNeeded = new std::vector<Voxel>();
+    voxelsNeeded->clear();
+    voxelsNeeded->push_back(Voxel("A",voxeloracle->getZoomedIn()));
+    voxelsNeeded->push_back(Voxel("B",voxeloracle->getZoomedIn()));
+    voxelsNeeded->push_back(Voxel("C",voxeloracle->getZoomedIn()));
+    voxeloracle->setManualControl(*voxelsNeeded);
+
+    hierarchyRenderer->renderDynamicVoxels();
+
+
+
+    voxeloracle->setZoomedIn(0);
+    voxelsNeeded->clear();
+    voxelsNeeded->push_back(Voxel("A",voxeloracle->getZoomedIn()));
+    voxelsNeeded->push_back(Voxel("B",voxeloracle->getZoomedIn()));
+    voxelsNeeded->push_back(Voxel("C",voxeloracle->getZoomedIn()));
+    voxeloracle->setManualControl(*voxelsNeeded);
+
+    hierarchyRenderer->renderDynamicVoxels();
+
+}
+
+
+void test3()
+{
+    // Voxel Oracle
+    int zoomedIn = 0;
+    Plane3D viewingPlane = Plane3D();
+    VoxelOracle* voxeloracle = new VoxelOracle(zoomedIn,viewingPlane);
+
+    //Cache maintain
+    int cacheCapacity = 10;
+    int hierarchySize = 10;
+    CacheMaintain* cachemaintain = new
+CacheMaintain(cacheCapacity,hierarchySize);
+
+    //Hierarchy Renderer
+    HierarchyRenderer* hierarchyRenderer = new
+HierarchyRenderer(voxeloracle,cachemaintain);
+
+
+
+    voxeloracle->setZoomedIn(2);
+    std::vector<Voxel>* voxelsNeeded = new std::vector<Voxel>();
+    voxelsNeeded->clear();
+    voxelsNeeded->push_back(Voxel("A",voxeloracle->getZoomedIn()));
+    voxelsNeeded->push_back(Voxel("B",voxeloracle->getZoomedIn()));
+    voxelsNeeded->push_back(Voxel("C",voxeloracle->getZoomedIn()));
+    voxeloracle->setManualControl(*voxelsNeeded);
+
+    hierarchyRenderer->renderDynamicVoxels();
+
+
+
+    voxeloracle->setZoomedIn(0);
+    voxelsNeeded->clear();
+    voxelsNeeded->push_back(Voxel("A",voxeloracle->getZoomedIn()));
+    voxelsNeeded->push_back(Voxel("B",voxeloracle->getZoomedIn()));
+    voxelsNeeded->push_back(Voxel("C",voxeloracle->getZoomedIn()));
+    voxeloracle->setManualControl(*voxelsNeeded);
+
+    hierarchyRenderer->renderDynamicVoxels();
 
 }
